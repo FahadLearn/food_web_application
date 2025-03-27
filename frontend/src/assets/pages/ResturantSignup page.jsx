f7290cea93181d0bf71499bffd54e7e74908861e
FahadLearn <fahad.learn8@gmail.com>
2025-03-27 23:29:48 +0500

    kkk

--- frontend/src/assets/pages/ResturantSignup page.jsx
+++ frontend/src/assets/pages/ResturantSignup page.jsx
@@ -1,5 +1,5 @@
 import RestaurantSignup1 from "../components/Admin signup page-1";
-import RestaurantSignup2 from "../components/Admin signup page-2";
+// import RestaurantSignup2 from "../components/Admin signup page-2";
 import RestaurantSignup3 from "../components/Admin signup page-3";
 import RestaurantSignup4 from "../components/Admin signup page-4";
 import RestaurantSignup5 from "../components/Admin signup page-5";
